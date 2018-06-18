feddf1657de31dc709f9b949db9f9126961e7a3a
gambhirpulkit <gambhirpulkit@gmail.com>
2018-06-18 15:47:41 +0530

    cab sharing feature unabled without authentication

--- app/oler/serializers.py
+++ app/oler/serializers.py
@@ -22,14 +22,15 @@ class RideRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = RideHistory
-        fields = ('id', 'ride_type', 'active', 'd_id', 'driver_name', 'seats')
+        fields = ('id', 'ride_type', 'active', 'd_id', 'driver_name')
 
 
 class RideTrackSerializer(serializers.ModelSerializer):
     h_id = serializers.IntegerField(source="history_id", read_only=False, required=False)
     r_id = serializers.IntegerField(source="rider_id", read_only=False, required=False)
     rider_name = serializers.CharField(source="rider.first_name", read_only=False, required=False)
+    driver_name = serializers.CharField(source="history.driver.first_name", read_only=False, required=False)
 
     class Meta:
         model = RideTrack
-        fields = ('id', 'ride_state', 'h_id', 'r_id', 'rider_name')
+        fields = ('id', 'ride_state', 'h_id', 'r_id', 'rider_name', 'seats', 'driver_name')
--- app/oler/views.py
+++ app/oler/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.db.models import Sum, Q
 
 from .models import User, RideHistory, RideTrack
 from .serializers import UserSerializer, RideHistorySerializer, RideRequestSerializer, RideTrackSerializer
@@ -48,17 +49,25 @@ class RideRequestView(APIView):
         if ride_history.exists():
             ride_history = ride_history.first()
             ride_track = RideTrack.objects.filter(history_id=ride_history.id)
+            ride_track_seats = ride_track.aggregate(Sum('seats'))
+            rider_check = ride_track.filter(rider_id=rider_id)
+
             if ride_history.ride_type == RideHistory.MINI_GO:
                 return Response({"Driver not available"}, status=status.HTTP_303_SEE_OTHER)
-            elif ride_history.ride_type == RideHistory.SHARE and ride_history.seats + seats > 4:
-                return Response({"Driver not available"}, status=status.HTTP_303_SEE_OTHER)
+            elif ride_history.ride_type == RideHistory.SHARE and ride_track_seats['seats__sum'] + seats > 4:
+                return Response({"Seats not available"}, status=status.HTTP_303_SEE_OTHER)
             else:
-                updated_seats = ride_history.seats + seats
-                serializer = RideRequestSerializer(ride_history, data={"seats": updated_seats})
-                if serializer.is_valid():
-                    serializer.save()
-                    # Todo track serializer
-                return Response(serializer.data, status=status.HTTP_200_OK)
+                if rider_check.exists():
+                    return Response({"Cab already booked"}, status=status.HTTP_303_SEE_OTHER)
+                else:
+                    track_serializer = RideTrackSerializer(
+                        data={"seats": seats, "r_id": rider_id, "ride_state": RideTrack.REQUESTED,
+                              "h_id": ride_history.id}
+                    )
+                    if track_serializer.is_valid():
+                        track_serializer.save()
+
+                return Response(track_serializer.data, status=status.HTTP_200_OK)
         else:
             serializer = RideRequestSerializer(
                 data={"d_id": driver_id, "ride_type": ride_type, "seats": seats, "active": True}
@@ -67,7 +76,7 @@ class RideRequestView(APIView):
             if serializer.is_valid():
                 serializer.save()
                 track_serializer = RideTrackSerializer(
-                    data={"h_id": serializer.data['id'], "ride_state": RideTrack.REQUESTED, "r_id": rider_id})
+                    data={"h_id": serializer.data['id'], "ride_state": RideTrack.REQUESTED, "r_id": rider_id, "seats": seats})
                 if track_serializer.is_valid():
                     track_serializer.save()
 
@@ -78,50 +87,48 @@ class RideRequestView(APIView):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-# Update status of existing ride using ride history id
+# Logging status of existing ride using ride history id, ride_state and rider_id
 class RideTrackView(APIView):
     def post(self, request):
 
-        rider_id = request.data['rider_id']
+        rider_id = int(request.data['rider_id'])
         history_id = int(request.data['history_id'])
         ride_state = int(request.data['ride_state'])
 
-        ride_track = RideTrack.objects.filter(history=history_id, ride_state=ride_state).order_by('-created_at')
-
-        if ride_track.exists():
+        ride_track = RideTrack.objects.filter(history=history_id).order_by('-created_at')
+        ride_track_rider = ride_track.filter(rider_id=rider_id)
+        ride_track_state = ride_track_rider.filter(ride_state=ride_state)
+        if ride_track_state.exists():
             return Response({"Status already updated."}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            track_serializer = RideTrackSerializer(
-                data={"h_id": history_id, "ride_state": ride_state, "r_id": rider_id}
-            )
-            if track_serializer.is_valid():
-                track_serializer.save()
-                if ride_state == RideTrack.COMPLETED or ride_state == RideTrack.REJECTED:
-                    history = RideHistory.objects.get(pk=history_id)
-                    data = {"active": False}
-                    history_serializer = RideHistorySerializer(history, data)
-                    if history_serializer.is_valid():
-                        history_serializer.save()
-                return Response(track_serializer.data, status=status.HTTP_201_CREATED)
-
-# # Update profile of driver using his id
-# class DriverProfileUpdateView(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return Dr.objects.get(pk=pk)
-#         except RideHistory.DoesNotExist:
-#             raise Http404
-#
-#     def get(self, request, pk):
-#         history = self.get_object(pk)
-#         serializer = RideRequestSerializer(history)
-#         return Response(serializer.data)
-#
-#     def put(self, request, pk):
-#         history = self.get_object(pk)
-#         serializer = RideRequestSerializer(history, request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
+            ride_history = RideHistory.objects.filter(id=history_id).first()
+            if ride_history.ride_type == RideHistory.MINI_GO:
+                track_serializer = RideTrackSerializer(
+                    data={"h_id": history_id, "ride_state": ride_state, "r_id": rider_id}
+                )
+                if track_serializer.is_valid():
+                    track_serializer.save()
+                    if ride_state == RideTrack.COMPLETED or ride_state == RideTrack.REJECTED:
+                        history = RideHistory.objects.get(pk=history_id)
+                        data = {"active": False}
+                        history_serializer = RideHistorySerializer(history, data)
+                        if history_serializer.is_valid():
+                            history_serializer.save()
+                    return Response(track_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                track_serializer = RideTrackSerializer(
+                    data={"h_id": history_id, "ride_state": ride_state, "r_id": rider_id,
+                          "seats": ride_track_rider.first().seats}
+                )
+                if track_serializer.is_valid():
+                    track_serializer.save()
+                    ride_request_count = ride_track.filter(ride_state=RideTrack.REQUESTED).count()
+                    ride_over_count = ride_track.filter(
+                        Q(ride_state=RideTrack.REJECTED) | Q(ride_state=RideTrack.COMPLETED)).count()
+                    if ride_request_count == ride_over_count:
+                        history = RideHistory.objects.get(pk=history_id)
+                        data = {"active": False}
+                        history_serializer = RideHistorySerializer(history, data)
+                        if history_serializer.is_valid():
+                            history_serializer.save()
+                    return Response(track_serializer.data, status=status.HTTP_201_CREATED)
